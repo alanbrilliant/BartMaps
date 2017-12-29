ffbfeb1ea9f17f29120743e772564363e489d30d
missingno01 <rayaltenberg@gmail.com>
2017-12-29 03:28:10 -0800

    Completed App

--- app/src/main/java/com/harbinger/alan/bartmaps/MainActivity.java
+++ app/src/main/java/com/harbinger/alan/bartmaps/MainActivity.java
@@ -18,14 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -43,9 +36,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
